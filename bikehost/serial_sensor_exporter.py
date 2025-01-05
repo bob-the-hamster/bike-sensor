b9952aacb7e3b2017a72cc11f6acffd770af4b12
James Paige <Bob@HamsterRepublic.com>
2025-01-05 14:53:56 -0500

    Bike Sensor: Write metrics read from the serial port to a text file for node exporter to pick up

--- bikehost/serial_sensor_exporter.py
+++ bikehost/serial_sensor_exporter.py
@@ -8,8 +8,46 @@ scraped by prometheus or other tools.
 """
 
 import serial
-ser = serial.Serial("/dev/ttyACM0", timeout=10.0)
-while True:
-     cc = str(ser.readline())
-     print(cc[2:][:-5])
+import re
+import os
 
+########################################################################
+
+
+class SerialSensorExporter():
+    
+    def __init__(self):
+        self.textfile_name = "bike_sensor.txt"
+        self.dest_dir = "/tmp/node_exporter_textfiles/"
+        if not os.access(self.dest_dir, os.W_OK):
+            print(f"Textfile directory {repr(self.dest_dir)} is not writable. Falling back to the current directory")
+            self.dest_dir = "./"
+        
+        self.ser = serial.Serial("/dev/ttyACM0", timeout=10.0)
+        
+        # Letters numbers and underscore, followed by a number with an
+        # optional decimal part. We don't attempt to support timestamps
+        # in the data.
+        self.pattern = re.compile("^[a-zA-Z_][a-zA-Z0-9_]* \d+(\.\d+)?$")
+    
+    def run(self):
+        while True:
+             cc = str(self.ser.readline())
+             line = cc[2:][:-5]
+             print(line)
+             if self.pattern.match(line):
+                 self.write_metric(line)
+    
+    def write_metric(self, line):
+        filename = os.path.join(self.dest_dir, self.textfile_name)
+        with open(filename, "w") as f:
+            f.write(line + "\n")
+
+########################################################################
+
+def command_line_entry_point():
+    exporter = SerialSensorExporter()
+    exporter.run()
+
+if __name__ == "__main__":
+    command_line_entry_point()
